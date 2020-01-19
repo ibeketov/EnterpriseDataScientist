9092c406c39ab249960ebf60a7bb2ca42e45b94b
Ilya Beketov <ilya.beketov@gmail.com>
2020-01-19 21:20:56 +0100

    updates

--- support.py
+++ support.py
@@ -6,7 +6,7 @@ import seaborn as sns
 
 def colcheck(df):
     """
-    Prints short infor about columns
+    Prints short info about columns
     :param df: takes into a dataframe
     :return:
     """
@@ -15,3 +15,16 @@ def colcheck(df):
         print(df[col].head())
         print('Missing (%):', df[col].isnull().mean())
         print(10*'-')
+
+def showmissing(df, lower_thresh=0.0):
+    """
+    Shows a barplot of the missing values of all columns.
+    :param df: takes into a pandas dataframe.
+    :param lower_thresh: Allows to threshold the count of missing columns to gain greater insight
+    :return:
+    """
+    plt.figure(figsize=(16,10))
+    na_count = df.isna().sum()/df.shape[0]
+    na_count = na_count[na_count >= lower_thresh]
+    plot = sns.barplot(na_count.index.values, na_count)
+    plot.set_xticklabels(plot.get_xticklabels(), rotation=90)
